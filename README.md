ba60766cd107248249ef57dfe547132405e8933b
Dan Giffen <dangiffen@gmail.com>
2020-10-12 10:50:08 +0100

    variable and readme update

--- README.md
+++ README.md
@@ -2,12 +2,20 @@
 
 This module creates a small Windows VM for management and testing purposes.
 
-## Required input
+## Input
 
-azurerm_subnet_id
-resource_group_name
-location
-admin_username
-admin_password
-remote_mgt_ips
-tags
+| Name | Description | Type |
+| ---- | ----------- | ----
+| azurerm_subnet_id | subnet the vm nic is connected to | string |
+| resource_group_name  | resource group | string |
+| location | resource location | string |
+| admin_username | windows local admin username | string |
+| admin_password | windows local admin password | string |
+| remote_mgt_ips | source management IP address | string |
+| tags | resource tags | string |
+
+## Output
+
+| Name | Description |
+| ---- | ----------- |
+| | |
--- variables.tf
+++ variables.tf
@@ -1,1 +1,47 @@
-variable "region" {}
+variable "azurerm_subnet_id" {
+  description = "Subnet ID for VM NIC association"
+  type        = string
+}
+
+variable "admin_username" {
+  description = "Windows local administrator username"
+  type        = string
+}
+
+variable "admin_password" {
+  description = "Windows local administrator password"
+  type        = string
+}
+
+
+variable "location" {
+  description = "Regional Location"
+  type        = string
+}
+
+
+variable "remote_mgt_ips" {
+  description = "List of remote IPs that can access VM PIP on RDP Port"
+  type        = list(string)
+  default = ["194.105.149.2/32"] // ServiceWorks
+}
+
+
+variable "resource_group_name" {
+  description = "Resource Group for all module resources"
+  type        = string
+  default = "rg-cdw-mgmtvm"
+
+}
+
+variable "tags" {
+  description = "Resource tags deployed with this module"
+  type        = map(string)
+  default = {
+    deployment = "terraform"
+    owner      = "cdw"
+  }
+
+}
+
+    
